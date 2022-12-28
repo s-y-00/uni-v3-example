0e34e948cdbde22a8b0e237f3ca856334597d72b
s-y-00 <homhom1004@gmail.com>
2022-12-28 23:07:52 +0900

    first commit

---
+++ getPoolImmutables.ts
@@ -0,0 +1,50 @@
+import { ethers } from 'ethers'
+
+const provider = new ethers.providers.JsonRpcProvider('https://eth.llamarpc.com')
+
+const poolAddress = '0x8ad599c3A0ff1De082011EFDDc58f1908eb6e6D8'
+
+const poolImmutablesAbi = [
+  'function factory() external view returns (address)',
+  'function token0() external view returns (address)',
+  'function token1() external view returns (address)',
+  'function fee() external view returns (uint24)',
+  'function tickSpacing() external view returns (int24)',
+  'function maxLiquidityPerTick() external view returns (uint128)',
+]
+
+const poolContract = new ethers.Contract(poolAddress, poolImmutablesAbi, provider)
+
+interface Immutables {
+  factory: string
+  token0: string
+  token1: string
+  fee: number
+  tickSpacing: number
+  maxLiquidityPerTick: number
+}
+
+async function getPoolImmutables() {
+  const [factory, token0, token1, fee, tickSpacing, maxLiquidityPerTick] = await Promise.all([
+    poolContract.factory(),
+    poolContract.token0(),
+    poolContract.token1(),
+    poolContract.fee(),
+    poolContract.tickSpacing(),
+    poolContract.maxLiquidityPerTick(),
+  ])
+
+  const immutables: Immutables = {
+    factory,
+    token0,
+    token1,
+    fee,
+    tickSpacing,
+    maxLiquidityPerTick,
+  }
+  return immutables
+}
+
+getPoolImmutables().then((result) => {
+  console.log(result)
+})
